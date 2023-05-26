59fb76d9b5541ad9c9837afac44abaf6f012c28d
Sophia Müller-Dott <sophia.md@hotmail.de>
2023-05-26 08:41:19 +0200

    added ptm-sea for activity estimation

---
+++ workflow/scripts/03.1_activity_estimation_ptmsea.R
@@ -0,0 +1,36 @@
+if(exists("snakemake")){
+  dataset <- snakemake@input$file_dataset
+  dataset_name <- snakemake@wildcards$dataset
+  PKN <- snakemake@input$file_PKN
+  PKN_name <- snakemake@wildcards$PKN
+  log <- snakemake@output$rds
+  output_folder <- snakemake@params$output_folder
+}else{
+  dataset <- "results/datasets/gbm.gct"
+  dataset_name <- "gbm"
+  PKN <- "results/prior/ptm-sea/GPS.gmt"
+  PKN_name <- "GPS"
+  output_folder <- "results/activity_scores_ptmsea"
+  log <- "results/activity_scores_ptmsea/log/gbm_GPS.log"
+  if(!require("ssGSEA2")) remotes::install_github('nicolerg/ssGSEA2', upgrade='never')
+}
+
+## Libraries ---------------------------
+library(ssGSEA2)
+library(tidyselect)
+
+## Run ssGSEA/PTM-SEA ---------------------------
+res <- run_ssGSEA2(dataset,
+                  output.prefix = paste(dataset_name, PKN_name, sep = "_"),
+                  gene.set.databases = PKN,
+                  output.directory = output_folder,
+                  sample.norm.type = "none",
+                  weight = 0.75,
+                  correl.type = "rank",
+                  statistic = "area.under.RES",
+                  output.score.type = "NES",
+                  nperm = 100,
+                  min.overlap = 5,
+                  extended.output = TRUE,
+                  global.fdr = FALSE,
+                  log.file = log)
